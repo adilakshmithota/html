682fedf74f7e1ad4c84f4523d4229dde82510221
adilakshmithota <140188328+adilakshmithota@users.noreply.github.com>
2024-11-26 21:19:07 +0530

    Add files via upload

---
+++ example10.js
@@ -0,0 +1,12 @@
+let company = {
+    name: "Google",
+    employees: [
+    { name: "lakshmi",
+        position:"Developer"
+},
+{ name: "cherry",
+    position: "designer" }
+]
+};
+console.log(company.employees[0].name);
+console.log(company.employees[1].position);
